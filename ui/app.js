4423fd5edef6ce32da0eea7f55dc4f08f6ebbc4c
addigiffin <39894775+addigiffin@users.noreply.github.com>
2025-11-13 17:47:51 -0500

    Create app.js
    Control center for firebase configurations, which we can use to access other API's safely through firebase cloud services (from what I read, can change if necessary)

---
+++ ui/app.js
@@ -0,0 +1,9 @@
+// TODO: firebase config
+
+// TODO: signin authentication funtions called
+
+// TODO: firebase cloud storage functions
+
+// TODO: Set up Firebase cloud functions, where we can access API's from
+  // this is apparently easier and more secure to go this route
+
