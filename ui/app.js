6c0b76b1cd7c63519d3acc3dfba52c7ab8bfa1ae
addigiffin <39894775+addigiffin@users.noreply.github.com>
2025-11-13 21:38:32 -0500

    Update app.js
    Bare bones for Firebase Auth, need to figure out hosting first in order to continue

--- ui/app.js
+++ ui/app.js
@@ -1,4 +1,29 @@
-// TODO: firebase config
+// TODO: firebase authorization ---------
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDya-YLkvmgFjQFXdF8lQwK8Oc1-iZNnBo",
+    authDomain: "my-study-manager.firebaseapp.com",
+    projectId: "my-study-manager",
+    storageBucket: "my-study-manager.firebasestorage.app",
+    messagingSenderId: "43832292919",
+    appId: "1:43832292919:web:110c0bf47014e61a3171b2",
+    measurementId: "G-PCRYQTE7HY"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
+
+
 
 // TODO: signin authentication funtions called
 
